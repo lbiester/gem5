7e1de804a03577081e294b5f99b5384cc7ccebbd
Katie Matton <katiemat@umich.edu>
2019-10-31 19:30:46 -0400

    added code for plots
    Change-Id: If97b89b8045edb027581eb43ba4ee4ed3e53d6a7

--- util/stat_compiler.py
+++ util/stat_compiler.py
@@ -1,4 +1,8 @@
 import os
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
 
 def get_benchmark_to_filename():
     benchmark_to_filename = {}
@@ -16,6 +20,18 @@ def get_stat_for_benchmark(stat, btf):
                     stat_for_benchmarks[benchmark] = float(split_line[1])
     return stat_for_benchmarks
 
+def make_stat_hist(name, value_name, stat_for_benchmarks):
+    names = list(stat_for_benchmarks.keys())
+    ind = np.arange(len(names))
+    values = list(stat_for_benchmarks.values())
+    width = .35
+    plt.figure(figsize=(16,3))
+    plt.bar(ind, values, width)
+    plt.ylabel(value_name)
+    plt.xticks(ind, names)
+    plt.title('Histogram of {} for each Benchmark'.format(name))
+    plt.savefig('util/plots/{}.png'.format(name))
+    plt.clf()
 
 def main():
     btf = get_benchmark_to_filename()
@@ -23,10 +39,11 @@ def main():
     l1_miss_temp = get_stat_for_benchmark("system.cpu.dcache.overall_misses::total", btf)
     l2_miss_temp = get_stat_for_benchmark("system.l2cache.overall_misses::.cpu.data", btf)
     sim_insts = get_stat_for_benchmark("sim_insts", btf)
-
-    l1_miss = {1000 * l1_miss_temp[k] / sim_insts[k] for k in l1_miss_temp}
-    l2_miss = {1000 * l2_miss_temp[k] / sim_insts[k] for k in l2_miss_temp}
-
+    l1_miss = {k: 1000 * l1_miss_temp[k] / sim_insts[k] for k in l1_miss_temp}
+    l2_miss = {k: 1000 * l2_miss_temp[k] / sim_insts[k] for k in l2_miss_temp}
+    make_stat_hist("IPC", "instructions per cycle", ipc)
+    make_stat_hist("L1 CMPK", "misses per kilo-instruction", l1_miss)
+    make_stat_hist("L2 CMPK", "misses per kilo-instruction", l2_miss)
 
 if __name__ == "__main__":
     main()
