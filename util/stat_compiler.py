36bf2110f4108f2e0e01b66f9b872d40b8f11b9b
Amrit <aromana@umich.edu>
2019-10-31 20:16:59 -0400

    fixed plots

--- util/stat_compiler.py
+++ util/stat_compiler.py
@@ -2,12 +2,14 @@ import os
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 
-def get_benchmark_to_filename():
+def get_benchmark_to_filename(gem5_path):
+    spec_stats_path = os.path.join(gem5_path,'spec_stats')
     benchmark_to_filename = {}
-    for filename in os.listdir("spec_stats"):
-        benchmark_to_filename[filename.split(".")[1]] = os.path.join("spec_stats", filename)
+    for filename in os.listdir(spec_stats_path):
+        benchmark_to_filename[filename.split(".")[1]] = os.path.join(spec_stats_path, filename)
     return benchmark_to_filename
 
 def get_stat_for_benchmark(stat, btf):
@@ -20,30 +22,30 @@ def get_stat_for_benchmark(stat, btf):
                     stat_for_benchmarks[benchmark] = float(split_line[1])
     return stat_for_benchmarks
 
-def make_stat_hist(name, value_name, stat_for_benchmarks):
+def make_stat_hist(name, value_name, stat_for_benchmarks, gem5_path):
     names = list(stat_for_benchmarks.keys())
-    ind = np.arange(len(names))
-    values = list(stat_for_benchmarks.values())
-    width = .35
-    plt.figure(figsize=(16,3))
-    plt.bar(ind, values, width)
+    values = list(stat_for_benchmarks.values())    
+    g = sns.barplot(x=names,y=values,color='steelblue')
+    g.set_xticklabels(labels=names,rotation=60)
+    plt.tight_layout()
     plt.ylabel(value_name)
-    plt.xticks(ind, names)
     plt.title('Histogram of {} for each Benchmark'.format(name))
-    plt.savefig('util/plots/{}.png'.format(name))
+    plt.savefig(os.path.join(gem5_path,'util/plots/{}.png'.format(name)), bbox_inches = 'tight')
     plt.clf()
 
 def main():
-    btf = get_benchmark_to_filename()
+    #gem5_path = 'c:/users/amrit/documents/eecs573/gem5/'
+    gem5_path = '/gem5/'
+    btf = get_benchmark_to_filename(gem5_path)
     ipc = get_stat_for_benchmark("system.cpu.ipc", btf)
     l1_miss_temp = get_stat_for_benchmark("system.cpu.dcache.overall_misses::total", btf)
     l2_miss_temp = get_stat_for_benchmark("system.l2cache.overall_misses::.cpu.data", btf)
     sim_insts = get_stat_for_benchmark("sim_insts", btf)
     l1_miss = {k: 1000 * l1_miss_temp[k] / sim_insts[k] for k in l1_miss_temp}
     l2_miss = {k: 1000 * l2_miss_temp[k] / sim_insts[k] for k in l2_miss_temp}
-    make_stat_hist("IPC", "instructions per cycle", ipc)
-    make_stat_hist("L1 CMPK", "misses per kilo-instruction", l1_miss)
-    make_stat_hist("L2 CMPK", "misses per kilo-instruction", l2_miss)
+    make_stat_hist("IPC", "instructions per cycle", ipc, gem5_path)
+    make_stat_hist("L1 CMPK", "misses per kilo-instruction", l1_miss, gem5_path)
+    make_stat_hist("L2 CMPK", "misses per kilo-instruction", l2_miss, gem5_path)
 
 if __name__ == "__main__":
     main()
