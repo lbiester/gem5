41f7aa142743b4d1cf431de391e8d8e00387f05e
Amrit <aromana@umich.edu>
2019-11-30 12:05:15 -0500

    fixed sjeng vocabs and code to process them

--- python/util.py
+++ python/util.py
@@ -5,13 +5,13 @@ import numpy as np
 def load_vocab(address_file="vocab/sjeng_address.pkl", pc_file="vocab/sjeng_pc.pkl"):
     # TODO: consider loading other files?
     addresses, address_diffs = _read_address_file(address_file)
-    pcs, _ = _read_address_file(pc_file)
+    pcs, pc_diffs = _read_address_file(pc_file)
     # states are address, pc tuples
     return list(set(zip(addresses, pcs))), list(set(address_diffs))
 
 
 def _read_address_file(filename):
     with open(filename, "rb") as f:
-        loaded = pickle.load(f)
-        arr = loaded[0] + loaded[2:]
-        return np.cumsum(arr), arr[1:]
+        arr = pickle.load(f)
+    del arr[1]
+    return np.cumsum(arr)[1:], arr[1:]
