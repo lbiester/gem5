67ebfc4289fecfe29f1a8205c98eaa9acec4603f
Katie Matton <katiemat@umich.edu>
2019-12-02 21:28:36 -0500

    import
    Change-Id: I8643fbbf685edf891cbde51dec36755c4d4ac29d

--- python/torch_server.py
+++ python/torch_server.py
@@ -7,7 +7,7 @@ from python import util
 from python.bandits_prefetcher import BanditsPrefetcher
 from python.table_q_prefetcher import TableQLearningPrefetcher
 from python.table_bandits import ContextBandit
-from python.dqn_prefetcher import DQNPrefetcher
+from python.DQN_prefetcher import DQNPrefetcher
 
 prefetcher = None
 
